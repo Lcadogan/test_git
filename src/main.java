2e6af7764a90d013be0efc4669d4ee23a6d931f6
Timur Zalyaleev <zalyaleev.timur@gmail.com>
2023-02-16 16:58:26 +0300

    main changed

--- src/main.java
+++ src/main.java
@@ -3,7 +3,7 @@ public class main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("world");
+            System.out.println("Hello!");
         }
     }
 }
